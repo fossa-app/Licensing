0ed954b415b8c8420f932ce4e8100a941a2154ee
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-28 18:29:53 +0200

    Update SystemEntitlements.cs

--- Source/Licensing/SystemEntitlements.cs
+++ Source/Licensing/SystemEntitlements.cs
@@ -1,10 +1,13 @@
 namespace Fossa.Licensing;
 
+using TIKSN.Deployment;
+
 /// <summary>
 /// System Entitlements.
 /// </summary>
 /// <param name="EnvironmentName">Deployment environment name.</param>
 /// <param name="MaximumCompanyCount">Maximum Company Count.</param>
+[CLSCompliant(false)]
 public record SystemEntitlements(
-    string EnvironmentName,
+    EnvironmentName EnvironmentName,
     int MaximumCompanyCount);
